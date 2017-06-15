7785eb9c5dfe9a01ddb50f967474f74940ca19b7
kings <530675800@qq.com>
2017-06-16 00:01:21 +0800

    iserver demo

---
+++ Dev/mytest.js
@@ -0,0 +1,13 @@
+// js示例文件
+
+document.addEventListener('click', function() {
+	alert('Hello world!')
+}, false)
+
+function hello(name) {
+	alert('Hello, '+ name)
+}
+
+function he(name) {
+	alert('Hello, '+ name)
+}
